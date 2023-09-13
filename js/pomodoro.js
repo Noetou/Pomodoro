36e3fa74de9b18f1f81851cf7961607138460ac8
Noé Tourbillon <noe.tourbillon@etu.unicaen.fr>
2023-09-13 20:28:00 +0200

    final v1

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,6 +1,5 @@
-let mode = document.getElementById("mode");
+let mode = document.getElementById("workMode");
 let button = document.getElementById("startButton");
-
 let timer = document.getElementById("timer");
 
 let workTime = 25;
@@ -9,11 +8,11 @@ let breakTime = 5;
 let seconds = 0;
 
 
+let chrono;
 
+let secondsPrint;
 
-
-
-function chrono() {
+function workChrono() {
     if (workTime != 0) {
         if (seconds > 0) {
             seconds -= 1;
@@ -28,9 +27,54 @@ function chrono() {
         if (seconds > 0) {
             seconds -= 1;
         }
+        else {
+            breakMode = 5;
+            seconds = 0;
+            mode.id = 'breakMode';
+            document.body.style.backgroundColor = "lightgreen";
+            clearInterval(chrono);
+            chrono = setInterval(breakChrono, 1000);
+        }
 
     }
-    timer.innerHTML = ` ${workTime + " : " + seconds} `;
+    secondsPrint = seconds.toString().length < 2 ? '0' + seconds : seconds;
+    timer.innerHTML = `${workTime + " : " + secondsPrint}`;
+
+}
+
+
+function timerDisplay (chrono) {
+    let chronoString = chrono.toString()
+    chronoString = chronoString.length < 2 ? '0' + chronoString : chronoString
+    return chronoString
+  }
+
+function breakChrono() {
+    if (breakTime != 0) {
+        if (seconds > 0) {
+            seconds -= 1;
+        }
+        else {
+            seconds = 59;
+            breakTime -= 1;
+        }
+    }
+    else {
+        if (seconds > 0) {
+            seconds -= 1;
+        }
+        else {
+            workTime = 25;
+            mode.id = 'workMode';
+            document.body.style.backgroundColor = "rgb(173, 9, 9)";
+            clearInterval(chrono);
+            chrono = setInterval(workChrono, 1000);
+
+        }
+
+    }
+    secondsPrint = seconds.toString().length < 2 ? '0' + seconds : seconds;
+    timer.innerHTML = ` ${breakTime + " : " + secondsPrint} `;
 
 }
 
@@ -44,10 +88,10 @@ button.addEventListener('click', () => {
     if (button.innerHTML === `<em class="fa-solid fa-arrow-rotate-right fa-rotate-180"></em>`) {
         button.innerHTML = `<em class="fa-solid fa-play"></em>`;
         reset();
+
     }
     else {
         button.innerHTML = `<em class="fa-solid fa-arrow-rotate-right fa-rotate-180"></em>`;
-        setInterval(chrono, 1000);
-
+        chrono = setInterval(workChrono, 1000);
     }
 });
